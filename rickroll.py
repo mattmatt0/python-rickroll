58d927469c5d4b716e078f91dd9430a8f45d0594
Matthieu XILLO <matthieu.xillo@hotmail.com>
2020-08-03 22:16:07 +0200

    stored all functions (__import__ and getattr) in a list

--- rickroll.py
+++ rickroll.py
@@ -1,1 +1,2 @@
-getattr(__import__("webbrowser"), "open")("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+functions = [getattr, __import__]
+functions[0](functions[1]("webbrowser"), "open")("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
