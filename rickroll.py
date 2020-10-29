bd75f985afb3b462c1ef9cd352ce9dce3d658fe3
Matthieu XILLO <matthieu.xillo@hotmail.com>
2020-10-29 17:07:47 +0100

    Replaced double/triple characters with charater*n

--- rickroll.py
+++ rickroll.py
@@ -1,5 +1,5 @@
 functions = [getattr, __import__]
-webbrowser = "webbrowser"
+webbrowser = "we" + 'b'*2 + "rowser"
 openFunction = "open"
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+url = "h"+'t'*2 + "ps:" + '/'*2 + 'w'*3 + ".youtube.com/watch?v=dQw4w9WgXcQ"
 functions[0](functions[1](webbrowser), openFunction)(url)
